c3268869133bcb88d28f81171c36b65e0f6e9d38
unknown <bipson490@gmail.com>
2025-07-07 13:50:23 +0545

    next

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let input=document.getElementById("input");
-let submit=document.getElementsByClassName("submit-button");
-let display=document.getElementsByClassName("work-display");
-submit.addEventListener("click",addwork());
-function addwork(){
-    
+let submit=document.querySelector(".submit-button");
+submit.addEventListener("click",()=>{
+    let input1=document.querySelector("#input1").textContent;
+    addwork(input1);
+});
+function addwork(input1){
+    let li = document.createElement("li");
+    let display=document.querySelector("ol");
+    li.appendChild(document.createTextNode(input1));
+    display.appendChild(li);
 }
