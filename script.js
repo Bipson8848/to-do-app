cc7159e92b2d8b75a1c77ecfd6a1460f17992a9f
unknown <bipson490@gmail.com>
2025-07-07 10:17:50 +0545

    added new files for dev

---
+++ script.js
@@ -0,0 +1,7 @@
+let input=document.getElementById("input");
+let submit=document.getElementsByClassName("submit-button");
+let display=document.getElementsByClassName("work-display");
+submit.addEventListener("click",addwork());
+function addwork(){
+    
+}
